6c73735d9cce964a462651bd6492f096849ad24c
Shabana <122359738+Shabana007@users.noreply.github.com>
2023-01-15 03:40:41 +0500

    Add files via upload

---
+++ shbana/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+ name='shabanapackage',
+ version='0.1',
+ author="Shabana",
+ author_email="shabanadevelopers786@gmail.com",
+ description="This is Very Basic package including some basic functions",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
